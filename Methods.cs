5193e4a877cd6abc95fa8799bfe958f635abbfe0
JTCG7684 <59671433+JTCG7684@users.noreply.github.com>
2020-02-20 21:30:29 -0500

    Finished the "convert binary to hexadecimal" task!

--- Methods.cs
+++ Methods.cs
@@ -3,7 +3,114 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace methods {
+    public class Methods {
+        public char[] seperator = { ' ' };
+        public Int32 count = 8;
+        public List<char> hexas = new List<char>();
+        public int eightSlot;
+        public int fourSlot;
+        public int twoSlot;
+        public int oneSlot;
+        public int biTotal;
+        public char switcher;
 
+
+        public void ConvToHexa(string binary) {
+            char[] seperator = { ' ' };
+            Int32 count = 8;
+            String[] bits = binary.Split(seperator, count, StringSplitOptions.None);
+            List<char> hexas = new List<char>();
+
+            Console.Write("Your hexadecimal is: ");
+            foreach (string bit in bits) {
+                if (bit[0] == '1')
+                {
+                    eightSlot = 8;
+                }
+                else {
+                    eightSlot = 0;
+                }
+
+                if (bit[1] == '1')
+                {
+                    fourSlot = 4;
+                }
+                else
+                {
+                    fourSlot = 0;
+                }
+
+                if (bit[2] == '1')
+                {
+                    twoSlot = 2;
+                }
+                else
+                {
+                    twoSlot = 0;
+                }
+
+                if (bit[3] == '1')
+                {
+                    oneSlot = 1;
+                }
+                else
+                {
+                    oneSlot = 0;
+                }
+
+                biTotal = eightSlot + fourSlot + twoSlot + oneSlot;
+
+                
+
+                switch (biTotal) {
+                    case 10:
+                        switcher = 'A';
+                        Console.Write(switcher);
+                        break;
+
+                    case 11:
+                        switcher = 'B';
+                        Console.Write(switcher);
+                        break;
+
+                    case 12:
+                        switcher = 'C';
+                        Console.Write(switcher);
+                        break;
+
+                    case 13:
+                        switcher = 'D';
+                        Console.Write(switcher);
+                        break;
+
+                    case 14:
+                        switcher = 'E';
+                        Console.Write(switcher);
+                        break;
+
+                    case 15:
+                        switcher = 'F';
+                        Console.Write(switcher);
+                        break;
+                    default:
+                        Console.Write(biTotal);
+                        break;
+                }
+
+                hexas.Add(switcher);
+
+
+            }
+
+            int hexaLength = hexas.Count;
+            
+        }
+
+        public void ConvToBinary(string hexdec) { 
+            
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using methods;
+using System.Threading;
 
 namespace DecimalBinary_Converter
 {
@@ -11,6 +12,45 @@ namespace DecimalBinary_Converter
     {
         static void Main(string[] args)
         {
+            Methods m = new Methods();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\nHello! What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("\nHello {0}! This is a base converter. It will convert hexadecimals to decimal or binary, and vice versa for each one!", name);
+            Thread.Sleep(3000);
+
+            while (true)
+            {
+                Console.WriteLine("\nWhat is your value type? You can say, hexadecimal, decimal, or binary, as well as their capital and lowercase counterparts!");
+                Console.WriteLine("\nIf you type 'quit', then the program will stop.");
+                string valueType = Console.ReadLine();
+
+                if (valueType == "binary" || valueType == "Binary" || valueType == "BINARY")
+                {
+                    Console.WriteLine("\nYou have picked binary!");
+                    Thread.Sleep(1000);
+                    Console.WriteLine("\nWhat is your number? Please sepearate the number with a space every four bits. (ie 1001 1001)");
+                    string number = Console.ReadLine();
+                    m.ConvToHexa(number);
+                    Thread.Sleep(3000);
+                }
+                else if (valueType == "hexadecimal" || valueType == "Hexadecimal" || valueType == "HEXADECIMAL")
+                {
+                    Console.WriteLine("\nI am sorry, but this has not been set up yet.");
+                    continue;
+                }
+                else if (valueType == "decimal" || valueType == "Decimal" || valueType == "DECIMAL")
+                {
+                    Console.WriteLine("\nI am sorry, but this has not been set up yet.");
+                    continue;
+                }
+                else if (valueType == "quit" || valueType == "Quit" || valueType == "QUIT") {
+                    Console.WriteLine("\nHave a wonderful day!");
+                    Console.WriteLine("\nPress any button to quit...");
+                    Console.ReadLine();
+                    break;
+                }
+            }
         }
     }
 }
